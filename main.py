fac740e0ffb0c52d424d6e719080ec1f7f494641
unknown <danla2178@gmail.com>
2022-12-05 15:26:56 +0300

    Расширил и добавил в репозиторий main.py

--- main.py
+++ main.py
@@ -2,14 +2,15 @@
 from random import choices
 
 symbols = 'ABCDEFGHJKLMNOPQRSTUVWXYZ'
-
+symbolslow = 'abcdefghjklmnopqrstuvwxyz'
 chars = ''
 
 pwd_length = int(input('Введите длинну пароля: '))
 pwd_auto = (input('Сгенерировать пароль автоматически? (y,n): ') == 'y')
 
 for text, seq in(
-                 ('Включить верхний регистор',  symbols  ),
+                 ('Включить верхний регистор',     symbols  ),
+                ('Включить нижний регистор',     symbolslow  ),
     if pwd_auto or(input(text + ' (y,n): ') == 'y'):
        chars += seq
 
